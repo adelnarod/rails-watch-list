4461f2d4fb1a0b30f9d94478287b53f4ac279906
Adel Narod <adelnarod@gmail.com>
2022-05-26 16:59:31 +0400

    CRUD implemented

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,26 @@
+class BookmarksController < ApplicationController
+  def index
+  end
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(params_bookmark)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
